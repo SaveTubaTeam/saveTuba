63812f291954e6d3400579d45c4797354085f32b
ArturPoole24 <82685084+ArturPoole24@users.noreply.github.com>
2022-02-21 16:33:45 -0500

    Firebase and Login In Screen Shell created, no functionality yet...

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/infrastructure/navigation/screens/LoginScreen.js
+++ src/infrastructure/navigation/screens/LoginScreen.js
@@ -1,13 +1,100 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { Text, View, StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const onPress = () => console.log("yay");
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+      // TODO: Change View to SaveView or figureout universal safeView for all pages in navigation... i think Udemy tutorial has that implemented.
+    <KeyboardAvoidingView
+        style={styles.container}
+        behaivor="padding"
+    >
+        <View style={styles.inputContainer}>
+            <TextInput 
+                placeholder="Email"
+                value= {email}
+                onChangeText={text => setEmail(text)}
+                style={styles.input}
+            />
+            <TextInput 
+                placeholder="Password"
+                value= {password}
+                onChangeText={text => setPassword(text)}
+                style={styles.input}
+                secureTextEntry
+            />
+        </View>
+
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => { }}
+            >
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+            onPress={() => { }}
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>Register</Text>
+          </TouchableOpacity>
+
+        </View>
+      
+    </KeyboardAvoidingView>
   );
 };
 
 export default LoginScreen;
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    button: {
+        backgroundColor: '#0782F9',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: '#0782F9',
+        borderWidth: 2,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '700',
+        fontSize: 16,
+
+    },
+    buttonOutlineText: {
+        color: '#0782F9',
+        fontWeight: '700',
+        fontSize: 16,
+    },
+});
