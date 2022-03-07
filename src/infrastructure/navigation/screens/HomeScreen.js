2f493963213625321105116ca1806824ce29972f
nadjastojanovic <nadja.stojanovic@outlook.com>
2022-03-06 22:49:34 -0500

    implemented theme in home page and modified fontSizes

--- src/infrastructure/navigation/screens/HomeScreen.js
+++ src/infrastructure/navigation/screens/HomeScreen.js
@@ -1,17 +1,19 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
-import { auth } from '../../../../firebase';
-import { useNavigation } from '@react-navigation/native';
+import React from "react";
+import styled from "styled-components/native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { auth } from "../../../../firebase";
+import { useNavigation } from "@react-navigation/native";
 
+// Test implementing the theme
+const TitleText = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
 
 const HomeScreen = () => {
-
-
   return (
-    <View style={{alignContent: 'center', alignItems: 'center'}}>
-      <Text>
-        Hello
-      </Text>
+    <View style={{ alignContent: "center", alignItems: "center" }}>
+      <TitleText>Hello</TitleText>
     </View>
   );
 };
--- src/infrastructure/theme/fonts.js
+++ src/infrastructure/theme/fonts.js
@@ -12,8 +12,8 @@ export const fontWeights = {
 export const fontSizes = {
   caption: "12px",
   button: "14px",
-  body: "16px",
-  title: "20px",
+  body: "18px",
+  title: "24px",
   h5: "24px",
   h4: "34px",
   h3: "45px",
