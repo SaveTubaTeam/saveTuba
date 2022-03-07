31cb8cc71a1b7cc86478a31adf9857c4a3cee7f7
TrevorAL <tlachman4@gmail.com>
2022-03-07 13:48:01 -0500

    set up logout (will changed with nav update)

--- src/infrastructure/navigation/screens/Settings.js
+++ src/infrastructure/navigation/screens/Settings.js
@@ -1,21 +1,51 @@
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { SafeArea } from '../../../components/Utility/safe-area.components';
+import styled from "styled-components/native";
+import { List, Avatar } from "react-native-paper";
+import { auth } from '../../../../firebase';
+
+const SettingsItem = styled(List.Item)`
+  padding: 8px;
+`;
+const AvatarContainer = styled.View`
+  align-items: center;
+`;
 
 const Settings = () => {
+    const handleSignOut = () => {
+        auth
+            .signOut()
+            .then(() => {
+                navigation.replace("Login");
+            })
+            .catch(error => alert(error.message));
+    };
     const navigation = useNavigation();
     const Back = () => {
         navigation.replace("Home")
     }
     return (
         <View style={styles.container}>
-            <Text>Settings Page</Text>
+            <AvatarContainer>
+                <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                    <Avatar.Icon size={180} icon="head" backgroundColor="#2182BD" />
+                </TouchableOpacity>
+            </AvatarContainer>
             <TouchableOpacity
                 style={styles.button}
                 onPress={Back}
             >
                 <Text style={styles.buttonText}>Back</Text>
             </TouchableOpacity>
+            <List.Section>
+                <SettingsItem
+                    title="Logout"
+                    left={(props) => <List.Icon {...props} color="black" icon="door" />}
+                    onPress={handleSignOut}
+                />
+            </List.Section>
         </View>
     );
 };
