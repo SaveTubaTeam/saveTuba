fa414bd6e54d03ca03bc2148da777a3f1a90fcbf
Arlan Dawdy <arlandawdy@dyn218013.cc.lehigh.edu>
2022-03-29 15:00:35 -0400

    Functioning level navigation

--- src/features/Home/HomeScreen.js
+++ src/features/Home/HomeScreen.js
@@ -1,29 +1,77 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { SafeArea } from "../../components/safe-area.component";
 // import { auth } from "../../../firebase";
 import { useNavigation } from "@react-navigation/native";
+import { NavigationContainer, TabActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ScreenOne from '../Levels/LevelOne';
+import ScreenTwo from '../Levels/LevelTwo';
 
-const TitleText = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.title};
-`;
+const Stack = createNativeStackNavigator();
 
-const Body = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
+function HomeView() {
+  const navigation = useNavigation();
+  return (
+    <SafeArea style={{backgroundColor: "#CCE882"}}>
+      <ScrollView style={{ alignContent: "center"}}>
+        <TouchableOpacity onPress={() => navigation.push("ScreenOne")}
+          style={style.roundButton1}>
+          <Text>LevelOne</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("ScreenTwo")}>
+          <Text>LevelTwo</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeArea>
+  );
+}
 
 const HomeScreen = () => {
   return (
-    <SafeArea>
-      <View style={{ alignContent: "center", alignItems: "center" }}>
-        <TitleText>Title</TitleText>
-        <Body>Lorem ipsum dolor sit amet</Body>
-      </View>
-    </SafeArea>
+    <NavigationContainer independent ={true}>
+      <Stack.Navigator>
+        <Stack.Screen
+          name = "Home"
+          component = {HomeView}
+          options={{ headerShown: true}}
+        />
+        <Stack.Screen
+          name = "ScreenOne"
+          component = {ScreenOne}
+          options={{headerShown: true}}
+        />
+        <Stack.Screen
+          name = "ScreenTwo"
+          component = {ScreenTwo}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 export default HomeScreen;
+
+const style = StyleSheet.create({
+  roundButton1: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#748816',
+  },
+  roundButton2: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+    backgroundColor: '#748816'
+  }
+
+});
---
+++ src/features/Levels/LevelOne.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const LevelOne = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>LevelOne!</Text>
+    </View>
+  );
+};
+export default LevelOne;
