43dd07156fa6e6b6d85c5c7d3ef5dc9290190ae4
TrevorAL <tlachman4@gmail.com>
2022-04-12 00:23:11 -0400

    Starting to use FaIcons

--- src/features/Account/Components/general.component.js
+++ src/features/Account/Components/general.component.js
@@ -6,7 +6,7 @@ import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity, ScrollView } from "react-native";
 import { Avatar } from "react-native-paper";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faLeaf } from "@fortawesome/free-solid-svg-icons";
+import { faCircleInfo, faInfo, faLeaf } from "@fortawesome/free-solid-svg-icons";
 
 import { TitleText } from "../../../components/title-text.component";
 import { BodyText } from "../../../components/body-text.component";
@@ -32,14 +32,62 @@ export const GeneralCard = () => {
         <Card>
             <AvatarContainer>
                 <Spacer position="bottom" size="large">
-                    <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+                    <TitleText>General</TitleText>
+                </Spacer>
+                <BodyText>Flags</BodyText>
+                <Spacer position="bottom" size="medium" />
+                <Row>
+                    <TouchableOpacity onPress={() => navigation.navigate("AccountScreen")}>
+                        {/* This will need to also be a feature in the firebase - language */}
                         <Avatar.Icon
-                            size={180}
-                            icon="head"
+                            size={40}
+                            icon="flag"
                             backgroundColor={theme.colors.ui.tertiary}
                         />
                     </TouchableOpacity>
-                </Spacer>
+                    <Spacer position="right" size="medium" />
+                    <TouchableOpacity onPress={() => navigation.navigate("AccountScreen")}>
+                        {/* This will need to also be a feature in the firebase - language */}
+                        <Avatar.Icon
+                            size={40}
+                            icon="flag"
+                            backgroundColor={theme.colors.ui.tertiary}
+                        />
+                    </TouchableOpacity>
+                    <Spacer position="left" size="medium" />
+                    <TouchableOpacity onPress={() => navigation.navigate("AccountScreen")}>
+                        {/* This will need to also be a feature in the firebase - language */}
+                        <Avatar.Icon
+                            size={40}
+                            icon="flag"
+                            backgroundColor={theme.colors.ui.tertiary}
+                        />
+                    </TouchableOpacity>
+                </Row>
+                <Spacer size="medium" />
+                <BodyText>Sound Effects</BodyText>
+                <Spacer size="medium" />
+                <BodyText>Reminders</BodyText>
+                <Spacer size="medium" />
+                <Row>
+                    <BodyText>Help</BodyText>
+                </Row>
+                <Spacer size="medium" />
+
+                <Row>
+                    <BodyText>About</BodyText>
+                    <Spacer position="right" size="medium" />
+                    <TouchableOpacity onPress={() => navigation.navigate("AccountScreen")}>
+                        {/* This will need to also be a feature in the firebase - language */}
+                        <FontAwesomeIcon
+                            icon={faCircleInfo}
+                            size={16}
+                            color={theme.colors.ui.primary}
+                        />
+                    </TouchableOpacity>
+                </Row>
+                <Spacer size="medium" />
+
 
             </AvatarContainer>
         </Card>
--- src/features/Account/Components/personal.component.js
+++ src/features/Account/Components/personal.component.js
@@ -8,6 +8,7 @@ import { Avatar } from "react-native-paper";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faLeaf } from "@fortawesome/free-solid-svg-icons";
 
+
 import { TitleText } from "../../../components/title-text.component";
 import { BodyText } from "../../../components/body-text.component";
 import { Spacer } from "../../../components/spacer.component";
@@ -42,17 +43,6 @@ export function PersonalCard(props) {
                     </TouchableOpacity>
                 </Spacer>
                 <TitleText>{currentUser?.username}</TitleText>
-                {/* should be username */}
-                {/* We can soon have a part on login with username as well*/}
-                <Row>
-                    <BodyText color="primary">{currentUser?.currentScore}</BodyText>
-                    <FontAwesomeIcon
-                        icon={faLeaf}
-                        size={16}
-                        color={theme.colors.ui.primary}
-                    />
-                </Row>
-
             </AvatarContainer>
         </Card>
     );
