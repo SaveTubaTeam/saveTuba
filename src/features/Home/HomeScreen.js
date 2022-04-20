c7ae37e38e5b86b724702dca3919244618d9c464
nadjastojanovic <nadja.stojanovic@outlook.com>
2022-04-19 20:47:11 -0400

    background photos

--- src/features/Home/HomeScreen.js
+++ src/features/Home/HomeScreen.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
 import { SafeArea } from "../../components/safe-area.component";
 // import { auth } from "../../../firebase";
 import { useNavigation } from "@react-navigation/native";
-import { NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 //import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import ScreenOne from '../Levels/LevelOne';
-import ScreenTwo from '../Levels/LevelTwo';
-import ScreenThree from '../Levels/LevelThree';
-import ScreenFour from '../Levels/LevelFour';
-import ScreenFive from '../Levels/LevelFive';
-import ScreenSix from '../Levels/LevelSix';
-
+import ScreenOne from "../Levels/LevelOne";
+import ScreenTwo from "../Levels/LevelTwo";
+import ScreenThree from "../Levels/LevelThree";
+import ScreenFour from "../Levels/LevelFour";
+import ScreenFive from "../Levels/LevelFive";
+import ScreenSix from "../Levels/LevelSix";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,27 +30,46 @@ const ImageBg = styled.ImageBackground`
 function HomeView() {
   const navigation = useNavigation();
   return (
-    <SafeArea style={{backgroundColor: "#CCE882"}}>
-      <ScrollView style={{ alignContent: "center"}}>
-      <ImageBg source={require("../../../assets/homepagebackground.png")}>
-        <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("ScreenOne")}>
-          <Text style={style.baseText}>1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("ScreenTwo")}>
-          <Text style={style.baseText}>2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("ScreenThree")}>
-          <Text style={style.baseText}>3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("ScreenFour")}>
-          <Text style={style.baseText}>4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("ScreenFive")}>
-          <Text style={style.baseText}>5</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("ScreenSix")}>
-          <Text style={style.baseText}>6</Text>
-        </TouchableOpacity>
+    <SafeArea style={{ backgroundColor: "#CCE882" }}>
+      <ScrollView style={{ alignContent: "center" }}>
+        {/* Attribution: <a href="https://www.freepik.com/vectors/springtime">Springtime vector created by freepik - www.freepik.com</a> */}
+        <ImageBg source={require("../../../assets/homebg.png")}>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("ScreenOne")}
+          >
+            <Text style={style.baseText}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("ScreenTwo")}
+          >
+            <Text style={style.baseText}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("ScreenThree")}
+          >
+            <Text style={style.baseText}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("ScreenFour")}
+          >
+            <Text style={style.baseText}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("ScreenFive")}
+          >
+            <Text style={style.baseText}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("ScreenSix")}
+          >
+            <Text style={style.baseText}>6</Text>
+          </TouchableOpacity>
         </ImageBg>
       </ScrollView>
     </SafeArea>
@@ -56,47 +81,54 @@ const HomeScreen = () => {
 
   return (
     //<NavigationContainer independent ={true}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name = "Levels"
-          component = {HomeView}
-          options={{ headerShown: true,
-            headerStyle: {
-              backgroundColor: '#C6DC3B',
-            }
-          }}
-        />
-        <Stack.Screen
-          name = "ScreenOne"
-          component = {ScreenOne}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name = "ScreenTwo"
-          component = {ScreenTwo}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name = "ScreenThree"
-          component = {ScreenThree}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name = "ScreenFour"
-          component = {ScreenFour}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name = "ScreenFive"
-          component = {ScreenFive}
-          options={{headerShown: true}}
-        />
-        <Stack.Screen
-          name = "ScreenSix"
-          component = {ScreenSix}
-          options={{headerShown: true}}
-        />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Levels"
+        component={HomeView}
+        options={{
+          title: "Levels",
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#C6DC3B",
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+            fontFamily: "Gabriela_400Regular",
+            color: "#748816",
+          },
+        }}
+      />
+      <Stack.Screen
+        name="ScreenOne"
+        component={ScreenOne}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ScreenTwo"
+        component={ScreenTwo}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="ScreenThree"
+        component={ScreenThree}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="ScreenFour"
+        component={ScreenFour}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="ScreenFive"
+        component={ScreenFive}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="ScreenSix"
+        component={ScreenSix}
+        options={{ headerShown: true }}
+      />
+    </Stack.Navigator>
     //</NavigationContainer>
   );
 };
@@ -107,25 +139,25 @@ const style = StyleSheet.create({
   roundButton1: {
     width: 125,
     height: 125,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     left: 10,
     borderRadius: 100,
-    backgroundColor: '#CCE882'
+    backgroundColor: "#CCE882",
   },
   roundButton2: {
     width: 125,
     height: 125,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     left: 255,
     borderRadius: 100,
-    backgroundColor: '#CCE882'
+    backgroundColor: "#CCE882",
   },
   baseText: {
     fontSize: 50,
-    color: 'white',
-  }
+    color: "white",
+  },
 });
--- src/features/Levels/LevelOne.js
+++ src/features/Levels/LevelOne.js
@@ -1,23 +1,30 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Touchable, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Touchable,
+  Image,
+} from "react-native";
 import { SafeArea } from "../../components/safe-area.component";
 // import { auth } from "../../../firebase";
 import { useNavigation } from "@react-navigation/native";
-import { NavigationContainer} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LessononeScreen from '../Levels/levelOne/lessonOne/lessononeScreen';
-import LessontwoScreen from '../Levels/levelOne/lessonTwo/lessontwoScreen';
-import LessonthreeScreen from '../Levels/levelOne/lessonThree/lessonthreeScreen';
-import LessonfourScreen from '../Levels/levelOne/lessonFour/lessonfourScreen';
-import LessonfiveScreen from '../Levels/levelOne/lessonFive/lessonfiveScreen';
-import LessonsixScreen from '../Levels/levelOne/lessonSix/lessonsixScreen';
-import LessonsevenScreen from '../Levels/levelOne/lessonSeven/lessonsevenScreen';
-import LessoneightScreen from '../Levels/levelOne/lessonEight/lessoneightScreen';
-import LessonnineScreen from '../Levels/levelOne/lessonNine/lessonnineScreen';
+import LessononeScreen from "../Levels/levelOne/lessonOne/lessononeScreen";
+import LessontwoScreen from "../Levels/levelOne/lessonTwo/lessontwoScreen";
+import LessonthreeScreen from "../Levels/levelOne/lessonThree/lessonthreeScreen";
+import LessonfourScreen from "../Levels/levelOne/lessonFour/lessonfourScreen";
+import LessonfiveScreen from "../Levels/levelOne/lessonFive/lessonfiveScreen";
+import LessonsixScreen from "../Levels/levelOne/lessonSix/lessonsixScreen";
+import LessonsevenScreen from "../Levels/levelOne/lessonSeven/lessonsevenScreen";
+import LessoneightScreen from "../Levels/levelOne/lessonEight/lessoneightScreen";
+import LessonnineScreen from "../Levels/levelOne/lessonNine/lessonnineScreen";
 
 import { styles } from "styled-system";
-
 
 const Stack = createNativeStackNavigator();
 const ImageBg = styled.ImageBackground`
@@ -31,40 +38,68 @@ const Button = styled.TouchableOpacity`
   align-items: center;
 `;
 
-function LeveloneView(){
+function LeveloneView() {
   const navigation = useNavigation();
-  return(
-    <SafeArea style={{backgroundColor: "#CCE882"}}>
-      <ScrollView style={{ alignContent: "center"}}>
-        <ImageBg source={require("../../../assets/leveloneBack.png")}>
+  return (
+    <SafeArea style={{ backgroundColor: "#CCE882" }}>
+      <ScrollView style={{ alignContent: "center" }}>
+        {/* Attribution: <a href="https://www.freepik.com/vectors/spring-season">Spring season vector created by freepik - www.freepik.com</a> */}
+        <ImageBg source={require("../../../assets/lessonbg.png")}>
           <Button onPress={() => navigation.goBack()}>
             <Text style={style.backText}>Back</Text>
           </Button>
-          <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("LessonOne")}>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("LessonOne")}
+          >
             <Text style={style.baseText}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("Two")}>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("Two")}
+          >
             <Text style={style.baseText}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton3} onPress={() => navigation.push("Three")}>
+          <TouchableOpacity
+            style={style.roundButton3}
+            onPress={() => navigation.push("Three")}
+          >
             <Text style={style.baseText}>3</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("Four")}>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("Four")}
+          >
             <Text style={style.baseText}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("Five")}>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("Five")}
+          >
             <Text style={style.baseText}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("Six")}>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("Six")}
+          >
             <Text style={style.baseText}>6</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton3} onPress={() => navigation.push("Seven")}>
+          <TouchableOpacity
+            style={style.roundButton3}
+            onPress={() => navigation.push("Seven")}
+          >
             <Text style={style.baseText}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton2} onPress={() => navigation.push("Eight")}>
+          <TouchableOpacity
+            style={style.roundButton2}
+            onPress={() => navigation.push("Eight")}
+          >
             <Text style={style.baseText}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={style.roundButton1} onPress={() => navigation.push("Nine")}>
+          <TouchableOpacity
+            style={style.roundButton1}
+            onPress={() => navigation.push("Nine")}
+          >
             <Text style={style.baseText}>9</Text>
           </TouchableOpacity>
         </ImageBg>
@@ -75,64 +110,71 @@ function LeveloneView(){
 
 const LevelOne = () => {
   const navigation = useNavigation();
-  return(
+  return (
     //<NavigationContainer independent ={false}>
-      <Stack.Navigator>
-        <Stack.Screen
-            name = "Level One"
-            component = {LeveloneView}
-            options={{ headerShown: true,
-              headerStyle: {
-                backgroundColor: '#C6DC3B',
-              }
-            }}
-          />
-          <Stack.Screen
-            name = "LessonOne"
-            component = {LessononeScreen}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name = "Two"
-            component = {LessontwoScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Three"
-            component = {LessonthreeScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Four"
-            component = {LessonfourScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Five"
-            component = {LessonfiveScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Six"
-            component = {LessonsixScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Seven"
-            component = {LessonsevenScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Eight"
-            component = {LessoneightScreen}
-            options={{headerShown: true}}
-          />
-          <Stack.Screen
-            name = "Nine"
-            component = {LessonnineScreen}
-            options={{headerShown: true}}
-          />
-      </Stack.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Level One"
+        component={LeveloneView}
+        options={{
+          title: "Level 1",
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "#C6DC3B",
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+            fontFamily: "Gabriela_400Regular",
+            color: "#748816",
+          },
+        }}
+      />
+      <Stack.Screen
+        name="LessonOne"
+        component={LessononeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Two"
+        component={LessontwoScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Three"
+        component={LessonthreeScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Four"
+        component={LessonfourScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Five"
+        component={LessonfiveScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Six"
+        component={LessonsixScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Seven"
+        component={LessonsevenScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Eight"
+        component={LessoneightScreen}
+        options={{ headerShown: true }}
+      />
+      <Stack.Screen
+        name="Nine"
+        component={LessonnineScreen}
+        options={{ headerShown: true }}
+      />
+    </Stack.Navigator>
     //</NavigationContainer>
   );
 };
@@ -143,39 +185,39 @@ const style = StyleSheet.create({
   roundButton1: {
     width: 100,
     height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     left: 10,
     borderRadius: 100,
-    backgroundColor: '#CCE882'
+    backgroundColor: "#CCE882",
   },
   roundButton2: {
     width: 100,
     height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     left: 150,
     borderRadius: 100,
-    backgroundColor: '#CCE882'
+    backgroundColor: "#CCE882",
   },
   roundButton3: {
     width: 100,
     height: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     left: 280,
     borderRadius: 100,
-    backgroundColor: '#CCE882'
+    backgroundColor: "#CCE882",
   },
   baseText: {
     fontSize: 50,
-    color: 'white',
+    color: "white",
   },
   backText: {
     fontSize: 15,
-    color: 'white',
-  }
+    color: "white",
+  },
 });
