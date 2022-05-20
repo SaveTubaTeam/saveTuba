3a513c8f14064a973d4c3b2037abbed7df8cc966
nadjastojanovic <nadja.stojanovic@outlook.com>
2022-05-20 10:50:21 +0600

    profile username

--- src/features/Account/Components/personal.component.js
+++ src/features/Account/Components/personal.component.js
@@ -9,7 +9,14 @@ import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity, ScrollView } from "react-native";
 import { Avatar } from "react-native-paper";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faSolid, faCamera, faCircleInfo, faCircleQuestion, faInfo, faLeaf } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSolid,
+  faCamera,
+  faCircleInfo,
+  faCircleQuestion,
+  faInfo,
+  faLeaf,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { StyleSheet } from "react-native";
 import { TitleText } from "../../../components/title-text.component";
@@ -17,7 +24,6 @@ import { BodyText } from "../../../components/body-text.component";
 import { Spacer } from "../../../components/spacer.component";
 import { Card } from "../../../components/card.component";
 import { theme } from "../../../infrastructure/theme";
-
 
 const AvatarContainer = styled.View`
   align-items: center;
@@ -40,88 +46,83 @@ const InputContainer = styled.View`
 `;
 
 const Input = styled.TextInput`
+  font-size: 20px;
   font-family: ${(props) => props.theme.fonts.body};
-  color: ${(props) => props.theme.colors.text.tertiary};
-  background-color: ${(props) => props.theme.colors.ui.tertiary};
-  padding: ${(props) => props.theme.sizes[1]} ${(props) => props.theme.sizes[2]};
+  background-color: ${(props) => props.theme.colors.ui.secondary};
+  padding: ${(props) => props.theme.sizes[0]} ${(props) => props.theme.sizes[1]};
   border-radius: ${(props) => props.theme.sizes[2]};
   margin-top: ${(props) => props.theme.space[2]};
 `;
 
-
 export function PersonalCard(props) {
-    const navigation = useNavigation();
-    const { currentUser } = props;
+  const navigation = useNavigation();
+  const { currentUser } = props;
 
+  const [photo, setPhoto] = useState(null);
 
-    const [photo, setPhoto] = useState(null);
-
-    const getProfilePicture = async (currentUser) => {
-        const photoUri = await AsyncStorage.getItem(`${currentUser?.firstName}-photo`);
-        setPhoto(photoUri);
-    };
-
-    useFocusEffect(
-        React.useCallback(() => {
-            getProfilePicture(currentUser);
-        }, [currentUser])
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(
+      `${currentUser?.firstName}-photo`
     );
+    setPhoto(photoUri);
+  };
 
+  useFocusEffect(
+    React.useCallback(() => {
+      getProfilePicture(currentUser);
+    }, [currentUser])
+  );
 
-
-    return (
-        <Card>
-            <AvatarContainer>
-                <Spacer position="bottom" size="large">
-                    <Row>
-                        {/* <Avatar.Icon
+  return (
+    <Card>
+      <AvatarContainer>
+        <Spacer position="bottom" size="large">
+          <Row>
+            {/* <Avatar.Icon
                             size={180}
                             icon="head"
                             backgroundColor={theme.colors.ui.tertiary}
                         /> */}
 
-                        {!photo && (
-                            <Avatar.Icon
-                                size={180}
-                                icon="head"
-                                backgroundColor={theme.colors.ui.tertiary}
-                            />
-                        )}
-                        {photo && (
-                            <Avatar.Image
-                                size={180}
-                                source={{ uri: photo }}
-                                backgroundColor="#2182BD"
-                            />
-                        )}
+            {!photo && (
+              <Avatar.Icon
+                size={180}
+                icon="head"
+                backgroundColor={theme.colors.ui.tertiary}
+              />
+            )}
+            {photo && (
+              <Avatar.Image
+                size={180}
+                source={{ uri: photo }}
+                backgroundColor="#2182BD"
+              />
+            )}
 
+            <TouchableOpacity
+              onPress={() => navigation.navigate("CameraScreen")}
+            >
+              {/* This will need to also be a feature in the firebase - language */}
+              <FontAwesomeIcon
+                position="absolute"
+                icon={faCamera}
+                size={60}
+                top={20}
+                right={-10}
+                color={theme.colors.ui.primary}
+              />
+            </TouchableOpacity>
+          </Row>
+        </Spacer>
 
-
-                        <TouchableOpacity onPress={() => navigation.navigate("CameraScreen")}>
-                            {/* This will need to also be a feature in the firebase - language */}
-                            <FontAwesomeIcon
-                                position="absolute"
-                                icon={faCamera}
-                                size={60}
-                                top={20}
-                                right={-10}
-                                color={theme.colors.ui.primary}
-                            />
-                        </TouchableOpacity>
-                    </Row>
-                </Spacer>
-
-                <TitleText>Email: {currentUser?.email}</TitleText>
-                <Row>
-                    <TitleText>Change Username: </TitleText>
-                    <InputContainer>
-                        <Input
-                            placeholder={currentUser?.username}
-                        />
-                    </InputContainer>
-                </Row>
-            </AvatarContainer>
-        </Card>
-    );
-
+        <BodyText size="subtitle">Email: {currentUser?.email}</BodyText>
+        <Row>
+          <BodyText size="subtitle">Change Username: </BodyText>
+          <InputContainer>
+            <Input placeholder={currentUser?.username} />
+          </InputContainer>
+        </Row>
+      </AvatarContainer>
+    </Card>
+  );
 }
