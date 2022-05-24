d97a0fd5dedaee05890d820b9d36704c802b8da9
TrevorAL <tlachman4@gmail.com>
2022-05-24 10:19:48 +0600

    translations mergered

---
+++ src/components/Translations/i18n.js
@@ -0,0 +1,21 @@
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import kk from './kk.json';
+import ru from './ru.json';
+
+i18n.use(initReactI18next).init({
+    lng: 'en',
+    fallbackLng: 'en',
+    resources: {
+        en: en,
+        kk: kk,
+        ru: ru,
+    },
+    interpolation: {
+        escapeValue: false // react already safes from xss
+    }
+});
+
+export default i18n;
