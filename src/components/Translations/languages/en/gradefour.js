2df78df001ae066a22dfae883b95f05422b387d6
nadjastojanovic <nadja.stojanovic@outlook.com>
2022-07-20 12:56:56 -0400

    grade 5 lesson title translations

--- src/components/Translations/languages/en/gradefour.js
+++ src/components/Translations/languages/en/gradefour.js
@@ -7,4 +7,79 @@ export default {
   energy: "Energy and movement",
   ecology: "Ecology and \nsustainable development",
   discoveries: "Discoveries that \nchanged the world",
+
+  /* Chapter 1 lesson names */
+  c1l1: "The role of science in exploring the surrounding world",
+  c1l2: "Methods of observation and measurement in nature",
+  c1l3: "Processing and analysis of observations",
+
+  /* Chapter 2 lesson names */
+  c1l4: "Starry sky",
+  c1l5: "Sun and solar system",
+  c1l6: "Earth is our planet. The shape, size and movement of the earth",
+  c1l7: "How was our planet created?",
+  c1l8: "Structure and composition of the Earth",
+  c1l9: "Origin of life on Earth",
+  c1l10: "From the map to the plan of the area",
+  c1l11: "Conditional signs",
+  c1l12: "Polar and route survey of the site",
+  c1l13: "Continents and islands",
+  c1l14: "Ocean research",
+  c1l15: "Dispersal of man on earth",
+  c1l16: "Races of people",
+
+  /* Chapter 3 lesson names */
+  c1l17: "Physical body. Substance. Weight",
+  c1l18: "The structure of substances and diffusion",
+  c1l19: "Properties of substances",
+  c1l20: "Pure substances and mixtures",
+  c1l21: "Soluble and insoluble substances",
+  c1l22: "Natural and artificial substances",
+  c1l23: "Synthetic materials in our life. The safety of their use",
+  c1l24: "Variety of phenomena in nature",
+  c1l25: "In the world of physical and chemical phenomena",
+  c1l26: "Light phenomena",
+  c1l27: "Thermal phenomena",
+  c1l28: "Sound phenomena around us",
+  c1l29: "Electrical and magnetic phenomena",
+  c1l30: "Forces in nature",
+
+  /* Chapter 4 lesson names */
+  c1l31: "The cycle of matter in nature",
+  c1l32: "Mountain building",
+  c1l33: "Weathering of rocks",
+  c1l34: "Weather and climate",
+  c1l35: "Dangerous phenomena of nature",
+  c1l36: "Properties of living organisms",
+  c1l37: "Levels of organization of living organisms",
+  c1l38: "Photosynthesis in plant life",
+
+  /* Chapter 5 lesson names */
+  c1l39: "Types and sources of energy",
+  c1l40: "Interconversion of energy",
+  c1l41: "Alternative energy sources",
+  c1l42: "Rational use of energy and safety",
+  c1l43: "Movement in nature",
+  c1l44: "Equilibrium",
+
+  /* Chapter 6 lesson names */
+  c1l45: "Ecological system",
+  c1l46: "Natural and artificial ecosystems",
+  c1l47: "Factors affecting the life of ecosystems",
+  c1l48: "Living planet",
+  c1l49: "Variety of animals",
+  c1l50: "Relationships between animals",
+  c1l51: "Variety of plants and their role",
+  c1l52: "Fungi and microorganisms",
+  c1l53:
+    "Changes in natural ecosystems under the influence of human activities",
+  c1l54: "Ecological problems of Kazakhstan",
+  c1l55: "How to protect nature",
+  c1l56: "Red Book of Kazakhstan",
+  c1l57: "Rules of conduct in nature",
+
+  /* Chapter 7 lesson names */
+  c1l58: "Scientific discoveries and inventions that changed the world",
+  c1l59: "Discoveries and inventions of the future",
+  c1l60: "Inventions of the next twenty years that will change the world",
 };
