8b88de513aa75fde09047e2279c1dd553f5a163c
hjo224 <hjo224@lehigh.edu>
2023-05-22 17:33:14 +0600

    uwu

--- src/features/Home/HomeScreen.js
+++ src/features/Home/HomeScreen.js
@@ -36,7 +36,7 @@ function HomeView() {
           <Text style={style.baseText}>2</Text>
         </TouchableOpacity>
 
-        <Image
+        {/* <Image
           style={{
             position: "absolute",
             right: 20,
@@ -46,7 +46,7 @@ function HomeView() {
             height: 60,
           }}
           source={require("../../../assets/lock.png")}
-        /> 
+        />  */}
         <TouchableOpacity
           style={style.roundButton2}
           onPress={() => navigation.push("ChaptersHandler", { level: 2 })}
